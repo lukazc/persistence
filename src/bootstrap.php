7e7b74cd00441bd997464f415dda4d8fb4a70d74
zac <zac@nivas.hr>
2019-10-03 17:33:01 +0200

    Create EntityManager (use annotations)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
 // Doctrine autoloader
 require_once __DIR__.'/../vendor/autoload.php';
 // config
 require_once __DIR__.'/../config/config.php';
+
+$entitiesPath = array(__DIR__ . '/Blog/Entity');
+$config = Setup::createAnnotationMetadataConfiguration($entitiesPath, $dev);
+$entityManager = EntityManager::create($dbParams, $config);
